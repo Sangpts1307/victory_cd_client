742782de4a571e91cc352bac8f1f5a14ae8af8c4
NgocNguyen1503 <feyd153@gmail.com>
2025-11-07 00:37:14 +0700

    Add Login Template

---
+++ src/components/signinMethods.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="signin-methods">
+        <button @click="loginWithGoogle">Google</button>
+        <button @click="loginWithFacebook">Facebook</button>
+    </div>
+</template>
+
+<script setup>
+function loginWithGoogle() {
+    console.log('Đăng nhập bằng Google')
+}
+
+function loginWithFacebook() {
+    console.log('Đăng nhập bằng Facebook')
+}
+</script>
+
+<style scoped>
+.signin-methods {
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+}
+</style>
